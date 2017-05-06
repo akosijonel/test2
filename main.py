cd2c7067b7b4258d518a4588a6cb0ba32cf7e8eb
Yatten <jonel.mawirat@bugomi.com>
2017-05-06 08:51:15 +0000

    test

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pexpect
 child = pexpect.spawn("telnet 54.250.224.118 32773")
 child.sendline("\r\n")
 child.expect(">")
-child.sendline("enable")
+child.sendline("enable\r\n")
 child.expect("#")
-child.sendline("config terminal")
+child.sendline("config terminal\r\n")
 child.expect("#")
-child.sendline("hostname Router1")
+child.sendline("hostname Router1\r\n")
 child.expect("#")
-child.sendline("end")
+child.sendline("end\r\n")
 child.expect("#")
 
 
